a6233ab8f5153b63984451ecf34781b137d12b34
Mamunur Rashid <mamun.rashid.hridoy@gmail.com>
2022-06-18 12:19:33 +0600

    not found handler added

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,7 @@ import express from "express";
 import { json } from "body-parser";
 import config from "../config";
 import userRouter from "./route/user";
-import { errorHandler, currentUser } from "@mrticketing/common";
+import { errorHandler, currentUser, NotFoundError } from "@mrticketing/common";
 import cookieSession from "cookie-session";
 
 const app = express();
@@ -18,6 +18,10 @@ app.use(
 app.use(json());
 app.use(currentUser(config.jwtKey));
 app.use(userRouter);
+
+app.all("*", (req, res) => {
+  throw new NotFoundError();
+});
 
 app.use(errorHandler);
 
